1c4225fc78d4e093efda85c594f29d01d6d5ffb9
bertilegh <gorm.lyman@elevmail.stockholm.se>
2024-11-22 10:15:32 +0100

    Formatted code according to standards

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ pygame.init()
 
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
-ship_image = pygame.image.load('PiratesTotalShipSide2.png')
-ship_image1 = pygame.image.load('PiratesTotalShipSide1.png')
-ship_image2 = pygame.image.load('PiratesTotalShipSide2.png')
-ship_image3 = pygame.image.load('PiratesTotalShipFront.png')
-ship_image4 = pygame.image.load('PiratesTotalShipBack.png')
+ship_image = pygame.image.load("PiratesTotalShipSide2.png")
+ship_image1 = pygame.image.load("PiratesTotalShipSide1.png")
+ship_image2 = pygame.image.load("PiratesTotalShipSide2.png")
+ship_image3 = pygame.image.load("PiratesTotalShipFront.png")
+ship_image4 = pygame.image.load("PiratesTotalShipBack.png")
 
 ship_image = pygame.transform.scale(ship_image, (100, 100))
 ship_image1 = pygame.transform.scale(ship_image1, (100, 100))
@@ -37,6 +37,7 @@ direction = 0
 space = pymunk.Space()
 space.gravity = (0, 0)
 
+
 class Sprite:
     def __init__(self, x, y):
         self.image = ship_image
@@ -54,21 +55,33 @@ class Sprite:
         force_y = distance * math.sin(radians) * 50
         self.body.apply_force_at_local_point((force_x, force_y))
         current_velocity = self.body.velocity
-        speed = math.sqrt(current_velocity[0]**2 + current_velocity[1]**2)
+        speed = math.sqrt(current_velocity[0] ** 2 + current_velocity[1] ** 2)
         if speed > self.max_speed:
-            normalized_velocity = (current_velocity[0] / speed, current_velocity[1] / speed)
-            self.body.velocity = (normalized_velocity[0] * self.max_speed, normalized_velocity[1] * self.max_speed)
+            normalized_velocity = (
+                current_velocity[0] / speed,
+                current_velocity[1] / speed,
+            )
+            self.body.velocity = (
+                normalized_velocity[0] * self.max_speed,
+                normalized_velocity[1] * self.max_speed,
+            )
+
     def draw(self, surface, camera_x, camera_y):
-        self.rect.topleft = (self.body.position.x - camera_x, self.body.position.y - camera_y)
+        self.rect.topleft = (
+            self.body.position.x - camera_x,
+            self.body.position.y - camera_y,
+        )
         surface.blit(self.image, self.rect)
         if direction < 22.5 or direction > 157.5:
-           self.image = ship_image2
+            self.image = ship_image2
         if direction < 67.5 and direction > 22.5:
-           self.image = ship_image3
+            self.image = ship_image3
         if direction < 112.5 and direction > 67.5:
-           self.image = ship_image1
+            self.image = ship_image1
         if direction < 157.5 and direction > 112.5:
-           self.image = ship_image4
+            self.image = ship_image4
+
+
 class Box:
     def __init__(self, x, y):
         self.image = pygame.Surface((35, 35))
@@ -80,9 +93,13 @@ class Box:
         space.add(self.body, self.shape)
 
     def draw(self, surface, camera_x, camera_y):
-        self.rect.topleft = (self.body.position.x - camera_x, self.body.position.y - camera_y)
+        self.rect.topleft = (
+            self.body.position.x - camera_x,
+            self.body.position.y - camera_y,
+        )
         surface.blit(self.image, self.rect)
         pygame.draw.rect(surface, RED, self.rect, 2)
+
 
 cube = Box(300, 200)
 sprite = Sprite(375, 275)
